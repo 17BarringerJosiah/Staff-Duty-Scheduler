e0d337c04723be68a49ec4888e5c6509c3658b02
Josiah Barringer <59709224+17BarringerJosiah@users.noreply.github.com>
2024-10-02 12:07:02 -0400

    Add files via upload

---
+++ StaffDutyScheduler/StaffDutyScheduler/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerateSchedule = New System.Windows.Forms.Button()
+        Me.btnAddException = New System.Windows.Forms.Button()
+        Me.cmbStaffMembers = New System.Windows.Forms.ComboBox()
+        Me.dtpMonth = New System.Windows.Forms.DateTimePicker()
+        Me.dgvSchedule = New System.Windows.Forms.DataGridView()
+        Me.lblDutyMonth = New System.Windows.Forms.Label()
+        Me.lblInputExceptions = New System.Windows.Forms.Label()
+        Me.dtpExceptionDay = New System.Windows.Forms.DateTimePicker()
+        CType(Me.dgvSchedule, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGenerateSchedule
+        '
+        Me.btnGenerateSchedule.Location = New System.Drawing.Point(330, 219)
+        Me.btnGenerateSchedule.Name = "btnGenerateSchedule"
+        Me.btnGenerateSchedule.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerateSchedule.TabIndex = 1
+        Me.btnGenerateSchedule.Text = "&Generate"
+        Me.btnGenerateSchedule.UseVisualStyleBackColor = True
+        '
+        'btnAddException
+        '
+        Me.btnAddException.Location = New System.Drawing.Point(310, 163)
+        Me.btnAddException.Name = "btnAddException"
+        Me.btnAddException.Size = New System.Drawing.Size(110, 23)
+        Me.btnAddException.TabIndex = 2
+        Me.btnAddException.Text = "&Add Exception"
+        Me.btnAddException.UseVisualStyleBackColor = True
+        '
+        'cmbStaffMembers
+        '
+        Me.cmbStaffMembers.FormattingEnabled = True
+        Me.cmbStaffMembers.Location = New System.Drawing.Point(310, 110)
+        Me.cmbStaffMembers.Name = "cmbStaffMembers"
+        Me.cmbStaffMembers.Size = New System.Drawing.Size(121, 21)
+        Me.cmbStaffMembers.TabIndex = 3
+        '
+        'dtpMonth
+        '
+        Me.dtpMonth.Location = New System.Drawing.Point(264, 25)
+        Me.dtpMonth.Name = "dtpMonth"
+        Me.dtpMonth.Size = New System.Drawing.Size(200, 20)
+        Me.dtpMonth.TabIndex = 5
+        '
+        'dgvSchedule
+        '
+        Me.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSchedule.Location = New System.Drawing.Point(264, 248)
+        Me.dgvSchedule.Name = "dgvSchedule"
+        Me.dgvSchedule.Size = New System.Drawing.Size(218, 190)
+        Me.dgvSchedule.TabIndex = 6
+        '
+        'lblDutyMonth
+        '
+        Me.lblDutyMonth.AutoSize = True
+        Me.lblDutyMonth.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblDutyMonth.Location = New System.Drawing.Point(310, 9)
+        Me.lblDutyMonth.Name = "lblDutyMonth"
+        Me.lblDutyMonth.Size = New System.Drawing.Size(95, 13)
+        Me.lblDutyMonth.TabIndex = 8
+        Me.lblDutyMonth.Text = "Select Duty Month"
+        '
+        'lblInputExceptions
+        '
+        Me.lblInputExceptions.AutoSize = True
+        Me.lblInputExceptions.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblInputExceptions.Location = New System.Drawing.Point(319, 94)
+        Me.lblInputExceptions.Name = "lblInputExceptions"
+        Me.lblInputExceptions.Size = New System.Drawing.Size(86, 13)
+        Me.lblInputExceptions.TabIndex = 9
+        Me.lblInputExceptions.Text = "Input Exceptions"
+        '
+        'dtpExceptionDay
+        '
+        Me.dtpExceptionDay.Location = New System.Drawing.Point(264, 137)
+        Me.dtpExceptionDay.Name = "dtpExceptionDay"
+        Me.dtpExceptionDay.Size = New System.Drawing.Size(200, 20)
+        Me.dtpExceptionDay.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BackgroundImage = Global.StaffDutyScheduler.My.Resources.Resources.Golden_Griffen
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dtpExceptionDay)
+        Me.Controls.Add(Me.lblInputExceptions)
+        Me.Controls.Add(Me.lblDutyMonth)
+        Me.Controls.Add(Me.dgvSchedule)
+        Me.Controls.Add(Me.dtpMonth)
+        Me.Controls.Add(Me.cmbStaffMembers)
+        Me.Controls.Add(Me.btnAddException)
+        Me.Controls.Add(Me.btnGenerateSchedule)
+        Me.Name = "Form1"
+        Me.Text = "Staff Duty Scheduler"
+        CType(Me.dgvSchedule, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGenerateSchedule As Button
+    Friend WithEvents btnAddException As Button
+    Friend WithEvents cmbStaffMembers As ComboBox
+    Friend WithEvents dtpMonth As DateTimePicker
+    Friend WithEvents dgvSchedule As DataGridView
+    Friend WithEvents lblDutyMonth As Label
+    Friend WithEvents lblInputExceptions As Label
+    Friend WithEvents dtpExceptionDay As DateTimePicker
+End Class
---
+++ StaffDutyScheduler/StaffDutyScheduler/Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' List of staff members
+    Private Staff As New List(Of String)
+    ' Dictionary to hold exceptions (staff member -> list of unavailable days)
+    Private UnavailableDays As New Dictionary(Of String, List(Of Integer))
+    ' Random generator for schedule
+    Private rnd As New Random()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize DataGridView columns for Date and SD Officer
+        dgvSchedule.Columns.Add("Date", "Date (DD-MMM-YY)")
+        dgvSchedule.Columns.Add("Officer", "SD Officer")
+
+        ' Automatically load staff names from Personnel.txt on the desktop
+        ImportStaffFromDesktop()
+
+        ' Disable controls until staff is loaded
+        btnGenerateSchedule.Enabled = Staff.Count > 0
+        btnAddException.Enabled = Staff.Count > 0
+    End Sub
+
+    ' Function to import staff names from Personnel.txt on the desktop
+    Private Sub ImportStaffFromDesktop()
+        Try
+            ' Get the path to the desktop
+            Dim desktopPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            Dim staffFilePath As String = Path.Combine(desktopPath, "Personnel.txt")
+
+            ' Ensure the file exists before trying to load
+            If File.Exists(staffFilePath) Then
+                ' Clear existing staff and exceptions
+                Staff.Clear()
+                UnavailableDays.Clear()
+                cmbStaffMembers.Items.Clear()
+
+                ' Read staff names from the text file
+                For Each line As String In File.ReadLines(staffFilePath)
+                    Dim staffMember As String = line.Trim()
+                    If Not String.IsNullOrEmpty(staffMember) Then
+                        Staff.Add(staffMember)
+                        UnavailableDays(staffMember) = New List(Of Integer)() ' Initialize empty list of unavailable days
+                        cmbStaffMembers.Items.Add(staffMember) ' Add to ComboBox for exceptions
+                    End If
+                Next
+
+                If Staff.Count > 0 Then
+                    MessageBox.Show("Staff loaded successfully from Personnel.txt on your desktop.")
+                Else
+                    MessageBox.Show("No staff names found in Personnel.txt.")
+                End If
+            Else
+                MessageBox.Show("The file 'Personnel.txt' was not found on your desktop.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred while loading the staff: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Button to generate the schedule for the selected month
+    Private Sub btnGenerateSchedule_Click(sender As Object, e As EventArgs) Handles btnGenerateSchedule.Click
+        ' Clear previous schedule
+        dgvSchedule.Rows.Clear()
+
+        ' Get selected month and year
+        Dim selectedMonth As Integer = dtpMonth.Value.Month
+        Dim selectedYear As Integer = dtpMonth.Value.Year
+        Dim daysInMonth As Integer = DateTime.DaysInMonth(selectedYear, selectedMonth)
+
+        Dim dutySchedule As New Dictionary(Of Integer, String) ' Day -> Assigned Staff
+        Dim assignedStaff As New HashSet(Of String) ' To track which staff have been assigned
+
+        ' Shuffle the staff list for random assignment
+        Dim shuffledStaff As List(Of String) = Staff.OrderBy(Function() rnd.Next()).ToList()
+
+        ' Generate the schedule randomly, ensuring no one is assigned more than once and accounting for exceptions
+        For day As Integer = 1 To daysInMonth
+            ' Get a list of staff members available for the current day and not yet assigned
+            Dim availableStaff As List(Of String) = shuffledStaff.Where(Function(staffMember) Not UnavailableDays(staffMember).Contains(day) AndAlso Not assignedStaff.Contains(staffMember)).ToList()
+
+            If availableStaff.Count > 0 Then
+                ' Randomly select an available staff member for the current day
+                Dim selectedStaffIndex As Integer = rnd.Next(availableStaff.Count)
+                Dim selectedStaff As String = availableStaff(selectedStaffIndex)
+
+                ' Assign the staff to the current day
+                dutySchedule(day) = selectedStaff
+                assignedStaff.Add(selectedStaff) ' Mark them as assigned
+            Else
+                ' No available staff for this day
+                dutySchedule(day) = "No available staff"
+            End If
+        Next
+
+        ' Display the schedule in the DataGridView
+        For day As Integer = 1 To daysInMonth
+            Dim dutyDate As DateTime = New DateTime(selectedYear, selectedMonth, day)
+            dgvSchedule.Rows.Add(dutyDate.ToString("dd-MMM-yy"), dutySchedule(day))
+        Next
+    End Sub
+
+    ' Button to add exception for a staff member
+    Private Sub btnAddException_Click(sender As Object, e As EventArgs) Handles btnAddException.Click
+        Dim staffMember As String = cmbStaffMembers.SelectedItem.ToString()
+        Dim exceptionDay As DateTime = dtpExceptionDay.Value
+        Dim day As Integer = exceptionDay.Day ' Get the day part of the selected date
+
+        ' Add the unavailable day for the selected staff member
+        If Not UnavailableDays(staffMember).Contains(day) Then
+            UnavailableDays(staffMember).Add(day)
+            MessageBox.Show(staffMember & " is now unavailable on " & exceptionDay.ToString("dd-MMM-yyyy") & ".")
+        Else
+            MessageBox.Show(staffMember & " is already unavailable on " & exceptionDay.ToString("dd-MMM-yyyy") & ".")
+        End If
+    End Sub
+End Class
